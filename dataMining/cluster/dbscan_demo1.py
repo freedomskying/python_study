0e998dd6b4e07dc1ab9bc83f2d00cbfdc8fd2487
freedomskying <freedomskying@gmail.com>
2017-11-05 21:38:49 +0800

    test

---
+++ dataMining/cluster/dbscan_demo1.py
@@ -0,0 +1,69 @@
+import numpy as np
+import sklearn.cluster as skc
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import os
+
+#这个例子不算太好，因为虽然使用了dbscan算法进行聚类，但是由于是对上网时间（几点钟）进行聚类，且eps设定为0.01所以，最后的聚类其实就是直方图。
+
+mac2id = dict()
+onlinetimes = []
+
+base_path = os.getcwd()
+base_path = base_path[:base_path.find("python_study\\") + 13]
+file_path = base_path + "dataMining\\files\\cluster\\TestData.txt"
+
+#学生月上网时间分布-TestData
+f = open(file_path, encoding='utf-8')
+for line in f:
+    #读取每条记录中的mac地址、开始上网时间、上网时长
+    mac = line.split(',')[2]
+    onlinetime = int(line.split(',')[6])
+    starttime = int(line.split(',')[4].split(' ')[1].split(':')[0])
+
+    #查看MAC地址是否在MAC字典中，如果不在，则将MAC加入字典，MAC对应的value是加入时的顺序，从len(onlinetimes)中获取
+    #这里做了一个假设，就是所有的学生在上网时，并不重复，不存在22点上网半小时，然后23点再上网半小时的情况
+    #程序中，上网时长，如果发现一个id上网次数超过1次，会直接使用后一次覆盖前一次的上网时长。
+    #onlinetimes数组是按照mac2id中mac对应的value作为下标，存储开始时间和上网时长。
+    if mac not in mac2id:
+        mac2id[mac] = len(onlinetimes)
+        onlinetimes.append((starttime, onlinetime))
+    else:
+        onlinetimes[mac2id[mac]] = [(starttime, onlinetime)]
+#print(mac2id)
+#print(onlinetimes)
+
+#将onlinetimes从元组数据类型reshape成为列表数据类型，存储在real_X中
+#()代表元组，[]代表列表，{}代表字典
+real_X = np.array(onlinetimes).reshape((-1, 2))
+
+#以列表形式，获取上网时长，存入X中
+X = real_X[:, 0:1]
+
+#调用dbscan算法进行训练
+#eps：两个样本被看做邻居节点的最大距离
+#min_samples：簇的样本数
+db = skc.DBSCAN(eps=0.01, min_samples=20).fit(X)
+labels = db.labels_
+
+#使用dbscan算法进行聚类后，会将异常点排除在外，在打印labels时会看到部分点的标签为-1，就是异常点。
+print('Labels:')
+print(labels)
+
+#计算噪声数据的比例
+raito = len(labels[labels[:] == -1]) / len(labels)
+print('Noise raito:', format(raito, '.2%'))
+
+#计算簇的个数，排除噪声后的簇的个数
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+
+#将每个簇进行打印
+for i in range(n_clusters_):
+    print('Cluster ', i, ':')
+    print(list(X[labels == i].flatten()))
+
+plt.hist(X, 24)
+plt.show()
---
+++ dataMining/cluster/kmeans_demo1.py
@@ -0,0 +1,41 @@
+import sys, os
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+def loadData(file_path):
+    fr = open(file_path, 'r+')
+    lines = fr.readlines()
+    retData = []
+    retCityName = []
+    for line in lines:
+        items = line.strip().split(",")
+        retCityName.append(items[0])
+        retData.append([float(items[i]) for i in range(1, len(items))])
+    return retData, retCityName
+
+
+if __name__ == '__main__':
+
+    #获取聚类文件地址
+    base_path = os.getcwd()
+    base_path = base_path[:base_path.find("python_study\\") + 13]
+    data, cityName = loadData(base_path + "dataMining\\files\\cluster\\city.txt")
+
+    #使用KMeans算法进行聚类，聚类数为3，
+    km = KMeans(n_clusters=3)
+    #label为每条记录对应的聚类标签，该例中为城市对应的聚类标签（0,1,2）
+    label = km.fit_predict(data)
+
+    #km.cluster_centers_为聚类中心，如果为三类，则有三个聚类中心，对应着每一类数据每一个维度的欧氏距离平均值
+    #np.sum，对聚类中心的点的各个维度的值进行求和，该例中表示对每个分类的聚类中心的各个维度值求和
+    expenses = np.sum(km.cluster_centers_, axis=1)
+
+    #print(km.cluster_centers_)
+    # print(expenses)
+    CityCluster = [[], [], []]
+    for i in range(len(cityName)):
+        CityCluster[label[i]].append(cityName[i])
+    for i in range(len(CityCluster)):
+        print("Expenses:%.2f" % expenses[i])
+        print(CityCluster[i])
