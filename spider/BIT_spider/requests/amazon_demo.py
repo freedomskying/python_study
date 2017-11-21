249f3b28f880f3985242370875e60a6b691cc988
freedomskying <freedomskying@gmail.com>
2017-11-21 19:55:55 +0800

    增加爬虫程序

---
+++ spider/BIT_spider/requests/amazon_demo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# 导入requests库
+import requests
+
+url = "https://www.amazon.cn/gp/product/B071SDP8PC/ref=amb_link_10?ie=UTF8&m=A1AJ19PSB66TGU&pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=merchandised-search-1&pf_rd_r=7ZPBARPAKPM7B9W5EW8H&pf_rd_r=7ZPBARPAKPM7B9W5EW8H&pf_rd_t=101&pf_rd_p=b9c6613d-af56-4edb-ad84-848bf4d9e2a7&pf_rd_p=b9c6613d-af56-4edb-ad84-848bf4d9e2a7&pf_rd_i=658390051"
+
+try:
+    kv = {'user-agent': 'Mozilla/5.0'}
+    r = requests.get(url, headers=kv)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    print(r.text[:2000])
+except:
+    print("爬取失败")
