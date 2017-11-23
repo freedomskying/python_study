38967faa4afcd228d2c32fc0b98d09d54adda6c9
freedomskying <freedomskying@gmail.com>
2017-11-24 07:17:27 +0800

    test

--- spider/BIT_spider/bs4/bs_1.py
+++ spider/BIT_spider/bs4/bs_1.py
@@ -14,6 +14,53 @@ except:
     print("爬取失败")
 
 demo = r.text
-soup = BeautifulSoup(demo,"html.parser")
+soup = BeautifulSoup(demo, "html.parser")
 
-print(soup.prettify())
+# print(soup.prettify())
+
+# 标签打印
+print(soup.title)
+print(soup.a)
+print(soup.a.string)
+print(soup.a.name)
+print(soup.a.parent.name)
+print(soup.a.attrs)
+print(soup.a.attrs['href'])
+print(type(soup.a.attrs))
+print(type(soup.a))
+print(soup.p)
+print(soup.b.string)
+
+# 内容遍历
+print(soup.head)
+print(soup.head.contents)
+
+print(soup.body)
+print(soup.body.contents)
+print(soup.body.contents[1])
+print(len(soup.body.contents))
+
+for child in soup.body.children:
+    print(child)
+
+# 标签上行遍历
+print(soup.title.parent)
+
+for parent in soup.a.parents:
+    if parent is None:
+        print(parent)
+    else:
+        print(parent.name)
+
+#标签平行遍历
+print(soup.a.next_sibling)
+print(soup.a.next_sibling.next_sibling)
+print(soup.a.previous_sibling)
+print(soup.a.previous_sibling.previous_sibling)
+print(soup.a.parent)
+
+for sibling in soup.a.next_siblings:
+    print(sibling)
+
+for sibling in soup.a.previous_siblings:
+    print(sibling)
