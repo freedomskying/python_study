c01b39d798439cf0484371a62cd71a5684884c8a
freedomskying <freedomskying@gmail.com>
2017-11-27 15:29:26 +0800

    增加爬虫程序

--- spider/shitang/xinfadi_price_1_2000.py
+++ spider/shitang/xinfadi_price_1_2000.py
@@ -1,23 +1,47 @@
 # -*- coding: utf-8 -*-
 
 # 导入requests库
-import requests
 from bs4 import BeautifulSoup
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
+import requests
+import bs4
 
-file_xinfadi = open('d:\\xinfadi_1_2000.csv', 'w', encoding='utf-8')
 
-for page_num in range(1, 2):
+def getHTMLText(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
+    try:
+        r = requests.get(url, timeout=30, headers=headers)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
 
-    url_address = 'http://www.xinfadi.com.cn/marketanalysis/0/list/' + str(page_num) + '.shtml'
-    r = requests.get(url_address, headers=headers)  # 像目标url地址发送get请求，返回一个response对象
-    r.encoding = r.apparent_encoding
 
-    hq_table = BeautifulSoup(r.text, 'lxml').find_all('table', class_='hq_table')  # 获取网页中的class为hq_table的所有a标签
+def fill_price_list(html, pfile):
+    soup = BeautifulSoup(html, "html.parser")
+    i = 0
+    for tr in soup.find('table', class_='hq_table').children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr('td')
+            if i == 0:
+                i = i + 1
+                continue
+            i = i + 1
+            pfile.write(tds[0].string + "," + tds[1].string + "," + tds[2].string + "," + tds[3].string + "," + tds[
+                4].string + "," + tds[5].string + "," + tds[6].string + '\n')
 
-    #file_xinfadi.write(str(hq_table) + '\r\n')
-    print(hq_table)
 
-file_xinfadi.close()
+def main():
+    pfile = open('d:\\xinfadi_1_2000.csv', 'w', encoding='utf-8')
+
+    for page_num in range(1, 3):
+        url = 'http://www.xinfadi.com.cn/marketanalysis/0/list/' + str(page_num) + '.shtml'
+        html = getHTMLText(url)
+        fill_price_list(html, pfile)
+
+    pfile.close()
+
+
+main()
