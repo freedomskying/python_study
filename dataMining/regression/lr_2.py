1104a7bd6da908f3e5d2e75c6d09976a2337de2e
freedomskying <freedomskying@gmail.com>
2018-08-12 13:10:40 +0800

    test

--- dataMining/regression/lr_2.py
+++ dataMining/regression/lr_2.py
@@ -4,7 +4,7 @@ import os
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
 
-a=1
+a = 1
 base_path = os.getcwd()
 base_path = base_path[:base_path.find("python_study" + os.path.sep) + 13]
 data_path = base_path + os.path.sep + "dataMining" + os.path.sep + "files" + os.path.sep + "regression" \
@@ -20,20 +20,21 @@ for line in lines:
     datasets_X.append(int(items[0]))
     datasets_Y.append(int(items[1]))
 
-#获取 X轴面积 Y轴价格
+# 获取 X轴面积 Y轴价格
+# test
 length = len(datasets_X)
 
-#将X转换为二维数组，以符合线性回归拟合方程的需要
+# 将X转换为二维数组，以符合线性回归拟合方程的需要
 datasets_X = np.array(datasets_X).reshape([length, 1])
-#将Y转化为数组
+# 将Y转化为数组
 datasets_Y = np.array(datasets_Y)
 
-#设置matplotlib.pyplot的X轴数据，以X的最大值和最小值为边界，建立等差数列
+# 设置matplotlib.pyplot的X轴数据，以X的最大值和最小值为边界，建立等差数列
 minX = min(datasets_X)
 maxX = max(datasets_X)
 X = np.arange(minX, maxX).reshape([-1, 1])
 
-#degree = 2 表示为2次多项式，然后创建线性回归，使用线性模型学习X_poly和datasets_Y之间的关系
+# degree = 2 表示为2次多项式，然后创建线性回归，使用线性模型学习X_poly和datasets_Y之间的关系
 poly_reg = PolynomialFeatures(degree=2)
 X_poly = poly_reg.fit_transform(datasets_X)
 lin_reg_2 = linear_model.LinearRegression()
